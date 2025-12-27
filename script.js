034f4711d47456628e46bf9f7b0e58cff4d4d02e
Avijit_das2001 <55279276+Avijit-Das2301@users.noreply.github.com>
2025-12-28 00:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,15 +9,23 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
-
+	let ans="";
+	//Go from largest value to smallest
+	for (let i=0;i<=6;i++) {
+		const symbol=obj[i][0];
+		const value=obj[i][1];
+	
+	//Append symbol while it fits into num
+	while (num>=value) {
+		ans+=symbol;
+		num-=value;
+		}
+	}
+	return ans;
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
+console.log(convertToRoman(36));
 
 // do not edit below this line
 module.exports = convertToRoman
